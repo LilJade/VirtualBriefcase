48d86a458ec2b6ecdd4a9476664f0adb9b4bb952
itto <ittovalladares@hotmail.com>
2021-05-06 17:33:43 -0600

    Error de actulizar solucion

--- database/actualizarUsuario.go
+++ database/actualizarUsuario.go
@@ -4,76 +4,67 @@ import (
 	"context"
 
 	"time"
- 	"github.com/LilJade/virtualBriefcase/models"
+
+	"github.com/LilJade/virtualBriefcase/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-
 )
 
 //Actualizar el registro del usuario
 
-func ActualizarUsuario(U models.Usuario,ID string)(bool,error)  {
+func ActualizarUsuario(U models.Usuario, ID string) (bool, error) {
 
-	ctx ,cancel:=context.WithTimeout(context.Background(),15*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	database:=MongoConn.Database("bd")
-	col:=database.Collection("usuario")
+	database := MongoConn.Database("bd")
+	col := database.Collection("usuario")
 
 	registro := make(map[string]interface{}) // se crea arreglo para  almacenar datos temporalmente
 
-	if len(U.Nombre)>0{
-		registro["nombre"]=U.Nombre
+	if len(U.Nombre) > 0 {
+		registro["nombre"] = U.Nombre
 	}
 
-	if len(U.Apellidos)>0{
-		registro["apellidos"]=U.Apellidos
+	if len(U.Apellidos) > 0 {
+		registro["apellidos"] = U.Apellidos
 	}
 
-	if len(U.Direccion)>0{
-		registro["direccion"]=U.Direccion
+	if len(U.Direccion) > 0 {
+		registro["direccion"] = U.Direccion
 	}
 
-	registro["fechaNacimiento"]=U.FechaNacimiento
+	registro["fechaNacimiento"] = U.FechaNacimiento
 
-	if len(U.Email) >0{
-		registro["email"]=U.Email
+	if len(U.Email) > 0 {
+		registro["email"] = U.Email
 	}
-//se llama metodo encriptar para encriptar la contraseña  y actualizar
-	U.Password, _ = EncriptarPassword(U.Password)
-	if len(U.Password)>0{
-
-		registro["password"]=U.Password
+	if len(U.Avatar) > 0 {
+		registro["avatar"] = U.Avatar
 	}
-
-	if len(U.Avatar)>0 {
-		registro["avatar"]=U.Avatar
+	if len(U.Biografia) > 0 {
+		registro["biografia"] = U.Biografia
 	}
-	if len(U.Biografia)>0{
-		registro["biografia"]=U.Biografia
-	}
-	if len(U.Profesion)>0 {
-		registro["profesion"]=U.Profesion
+	if len(U.Profesion) > 0 {
+		registro["profesion"] = U.Profesion
 	}
 
+	registro["creado"] = U.Creado
 
-	registro["creado"]=U.Creado
+	updateString := bson.M{ //Convertir el valor bson
+		"$set": registro, //
+	}
 
+	objID, _ := primitive.ObjectIDFromHex(ID)
 
-		updateString:=bson.M{ //Convertir el valor bson
-			"$set":registro,//
-		}
+	filter := bson.M{"_id": bson.M{"$eq": objID}}
 
-		objID, _:=primitive.ObjectIDFromHex(ID)
-
-		filter :=bson.M{"_id": bson.M{"$eq": objID}}
-
-		_, err:=col.UpdateOne(ctx, filter, updateString)
+	_, err := col.UpdateOne(ctx, filter, updateString)
 
 	if err != nil {
-		return false,err
+		return false, err
 	}
 
-	return true,nil
+	return true, nil
 
 }
