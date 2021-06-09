60d3002ffe8e5e50473e641aba3e3024fb1f50a7
itto <ittovalladares@hotmail.com>
2021-06-09 00:57:40 -0600

    solucion de erro #1

--- database/insertoProyecto.go
+++ database/insertoProyecto.go
@@ -2,18 +2,20 @@ package database
 
 import (
 	"context"
+	"time"
+
 	m "github.com/LilJade/virtualBriefcase/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"time"
 )
 
+//InsertoProyecto es para insertar proyectos en la base
 func InsertoProyecto(p m.GraboProyecto) (string, bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	db := MongoConn.Database("bd")
-	col := db.Collection("proyecto")
+	col := db.Collection("proyectos")
 
 	registro := bson.M{
 		"userid":      p.UserID,
